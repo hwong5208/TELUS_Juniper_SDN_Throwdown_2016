642c15498f8a8482a8b97667278deae9f780b8db
Jopika <jojo12345678@gmail.com>
2016-07-23 19:22:12 -0700

    Added Sample output files, working on api_eventSwitch

--- src/api_eventSwitch.py
+++ src/api_eventSwitch.py
@@ -33,6 +33,9 @@ def redisChannel():
     return;
 
 
+# There should be a variable called 'downedLink' that contains the links that are down
+downedLink = ['10.210.16.1', '10.210.16.2'];
+
 # Start of API initializing
 url = "https://10.10.2.29:8443/oauth2/token"
 
@@ -43,13 +46,128 @@ authHeader= {"Authorization":"{token_type} {access_token}".format(**json_data)}
 
 r = requests.get('https://10.10.2.29:8443/NorthStar/API/v1/tenant/1/topology/1/te-lsps/', headers=authHeader, verify=False)
 
+our_lsps = [
+    'GROUP_SEVEN_SF_NY_LSP1',
+    'GROUP_SEVEN_SF_NY_LSP2',
+    'GROUP_SEVEN_SF_NY_LSP3',
+    'GROUP_SEVEN_SF_NY_LSP4',
+    'GROUP_SEVEN_NY_SF_LSP1',
+    'GROUP_SEVEN_NY_SF_LSP2',
+    'GROUP_SEVEN_NY_SF_LSP3',
+    'GROUP_SEVEN_NY_SF_LSP4']
+
 p = json.dumps(r.json())
 lsp_list = json.loads(p)
+
+def apiCall(lsp, ero):
+    # Fill only the required fields     
+    new_lsp = {}
+    for key in ('from', 'to', 'name', 'lspIndex', 'pathType'):
+        new_lsp[key] = lsp[key]
+    
+    new_lsp['plannedProperties'] = {
+        'ero': ero
+    }
+    
+    # print ero
+    # print 'welp'
+    response = requests.put('https://10.10.2.29:8443/NorthStar/API/v1/tenant/1/topology/1/te-lsps/' + str(new_lsp['lspIndex']), 
+                            json = new_lsp, headers=authHeader, verify=False)
+    # print response.text
+
+
+
+
+def switchERO(lsp, route):
+    lspName = lsp['name']
+    # print 'hello ', lspName
+    ero = {}
+    if route == 'r2':
+        if lspName[12:17] == 'SF_NY':
+            #dosomething
+            ero= [ 
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.16.1'},
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.17.2'}
+                           ]
+            print lsp['name'], route
+        elif lspName[12:17] == 'NY_SF':
+            #dosomething
+            ero= [
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.17.1'},
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.16.2'}
+                           ]
+            print lsp['name'], route
+    elif route == 'r1':
+        if lspName[12:17] == 'SF_NY':
+            #dosomething
+            ero= [ 
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.15.1'},
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.11.1'},
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.12.1'}
+                           ]
+            print lsp['name'], route
+        elif lspName[12:17] == 'NY_SF':
+            #dosomething
+            ero= [ 
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.12.2'},
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.11.2'},
+                            { 'topoObjectType': 'ipv4', 'address': '10.210.15.2'}
+                           ]
+            print lsp['name'], route
+    else:
+        print 'WARNING: SOMETHING WENT WRONG'
+
+
+    apiCall(lsp, ero)
+    # ero= [ 
+    #                 { 'topoObjectType': 'ipv4', 'address': '10.210.15.2'},
+    #                 { 'topoObjectType': 'ipv4', 'address': '10.210.13.2'},
+    #                 { 'topoObjectType': 'ipv4', 'address': '10.210.17.1'}
+    #                ]
+
+
+    # new_lsp = {}
+    # for key in ('from', 'to', 'name', 'lspIndex', 'pathType'):
+    #     new_lsp[key] = lsp[key]
+    
+    # new_lsp['plannedProperties'] = {
+    #     'ero': ero
+    # }
+    # print ero, '\n \n '
+    
+    # response = requests.put('https://10.10.2.29:8443/NorthStar/API/v1/tenant/1/topology/1/te-lsps/' + str(new_lsp['lspIndex']), 
+    #                         json = new_lsp, headers=authHeader, verify=False)
+
+
+
+
 # Find target LSP to use lspIndex
+
+# Assume there is two routes (r1, r2) that are arrays of 
+# strings specifying the routes (without terminating octet)
+
+r1 = ['10.210.16','10.210.17']
+r2 = ['10.210.15','10.210.11','10.210.12']
+
 for lsp in lsp_list:
-    if lsp['name'] == 'GROUP_SEVEN_SF_NY_LSP1':
-        print "found it!"
-        print lsp['liveProperties']['ero']
-        break
+    if lsp['name'] in our_lsps:
+        # print "found it!"
+        # print 'here is the current ero'
+        address_list = lsp['liveProperties']['ero']
+        route = 0
+        for adr in address_list:
+            address = adr['address']
+            if address[:-2] in r1:
+                route = 'r1'
+            elif address[:-2] in r2:
+                route = 'r2'
+            else:
+                route = 'error'
+            for deadAddress in downedLink:
+                if deadAddress == address:
+                    # Switch the LSP here
+                    switchERO(lsp, route);
+                    print "done!", lsp['name'], route, address[:-2], '\n'
+            # print address['address']
 
 print "hello everyone, so far so good!"
